318242751eeeb97ae7c4d76909538bfa76ace198
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-04-06 11:02:51 -0300

    ruleta para tp 2

---
+++ Ruleta_2/Ruleta.py
@@ -0,0 +1,40 @@
+import random
+
+# gracias chat gpt por genera esto
+numeros_rojos = [1, 3, 5, 7, 9, 12, 14, 16,
+                 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+numeros_negros = [2, 4, 6, 8, 10, 11, 13, 15,
+                  17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+
+
+class Color:
+    ROJO = 'rojo'
+    NEGRO = 'negro'
+
+
+class Ruleta:
+    def __init__(self, generate_number):
+        self.generate_number = generate_number
+        self.apuestas = []
+
+    def apostar_numero(self, numero: int, monto: int) -> int:
+        self.apuestas.append({'numeros': [numero], 'ganacia': monto*36})
+
+    def apostar_color(self, color, monto):
+        if (color == Color.ROJO):
+            self.apuestas.append(
+                {'numeros': numeros_rojos, 'ganacia': monto*2}
+            )
+        if (color == Color.NEGRO):
+            self.apuestas.append(
+                {'numeros': numeros_negros, 'ganacia': monto*2}
+            )
+
+    def tirar(self):
+        n = self.generate_number()
+        ganacia = 0
+        for a in self.apuestas:
+            if (n in a['numeros']):
+                ganacia += a['ganacia']
+        self.apuestas = []
+        return ganacia
---
+++ Ruleta_2/Ruleta_test.py
@@ -0,0 +1,62 @@
+import sys
+from Ruleta import Ruleta, Color
+
+
+def assert_condition(condition, desc):
+    try:
+        assert condition, desc
+        print('\033[32m', 'Passed: ', desc, '✅', '\033[0m')  # texto verde
+    except AssertionError as e:
+        print('\033[31m', 'FAILED: ', e, '❌', '\033[0m')  # texto rojo
+        sys.exit()
+# ----------------------------------------------------------------
+
+
+test_desc = 'apuesta $10 al 2 y sale el 2, deberia ganar $360'
+r = Ruleta(lambda: 2)
+r.apostar_numero(2, 10)
+assert_condition(r.tirar() == 360, test_desc)
+
+test_desc = 'apuesta $10 al 2 y sale el 10, no deberia ganar nada'
+r = Ruleta(lambda: 14)
+r.apostar_numero(2, 10)
+assert_condition(r.tirar() == 0, test_desc)
+
+test_desc = 'apuesta $100 al 3 y $200 al 5, sale el 3, deberia ganar $3600'
+r = Ruleta(lambda: 3)
+r.apostar_numero(3, 100)
+r.apostar_numero(5, 200)
+assert_condition(r.tirar() == 3600, test_desc)
+
+test_desc = 'apuesta $20 al 7 y $30 al 7 devuelta, sale el 7, deberia ganar $1800'
+r = Ruleta(lambda: 7)
+r.apostar_numero(7, 20)
+r.apostar_numero(7, 30)
+assert_condition(r.tirar() == 1800, test_desc)
+
+
+test_desc = 'apuesta $50 al negro y  $50 al rojo, gana $100'
+r = Ruleta(lambda: 3)
+r.apostar_color(Color.ROJO, 50)
+r.apostar_color(Color.NEGRO, 50)
+assert_condition(r.tirar() == 100, test_desc)
+
+test_desc = 'apuesta $10 al rojo y sale el 3(rojo), deberia ganar $20'
+r = Ruleta(lambda: 3)
+r.apostar_color(Color.ROJO, 10)
+assert_condition(r.tirar() == 20, test_desc)
+
+test_desc = 'apuesta $10 al negro y sale el 3(rojo), no deberia ganar nada'
+r = Ruleta(lambda: 3)
+r.apostar_color(Color.NEGRO, 10)
+assert_condition(r.tirar() == 0, test_desc)
+
+
+test_desc = 'si en la segunda tirada no apuesta no deberia ganar nada'
+r = Ruleta(lambda: 15)
+r.apostar_numero(15, 5000000)
+r.apostar_color(Color.NEGRO, 50000)
+primera = r.tirar()
+segunda = r.tirar()
+assert_condition(segunda == 0, test_desc)
+print("\033[1;32mEverything passed 👏\033[0m")
