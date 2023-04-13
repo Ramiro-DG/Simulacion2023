2df4480fc4f42617d10c4cde49c963640e086056
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-04-13 14:36:03 -0300

    distribuion de las gaancias luego de 100 tiradas, estrategia 3-2

--- Ruleta_2/Estrategia 3-2.py
+++ Ruleta_2/Estrategia 3-2.py
@@ -3,7 +3,8 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-iteraciones = 5000
+iteraciones = 200
+corridas = 3000
 arrayIteraciones = range(0, iteraciones)
 apuestaRojo = 300  # 3
 apuestaColumna = 200  # apuestaRojo*2/3  # 2}
@@ -12,7 +13,9 @@ vecesHastaGanar = [0]*iteraciones
 fig, ax = plt.subplots(figsize=(7, 4), constrained_layout=True)
 fig2, ax2 = plt.subplots(figsize=(7, 4), constrained_layout=True)
 
-for num in range(30):
+flujo_caja_finales = []
+
+for num in range(corridas):
     capitalInicial = 0.0
     flujoCaja = []
     frGanancia = []
@@ -25,8 +28,8 @@ for num in range(30):
         if (ganancia == 0):
             cantRondasSinGanar += 1
         else:
-           vecesHastaGanar[cantRondasSinGanar+1]+=1
-           cantRondasSinGanar = 0
+            vecesHastaGanar[cantRondasSinGanar+1] += 1
+            cantRondasSinGanar = 0
         capitalInicial = capitalInicial-(apuestaRojo+apuestaColumna)+ganancia
         flujoCaja.append(capitalInicial)
 
@@ -34,7 +37,7 @@ for num in range(30):
 
     result = np.divide(vecesHastaGanar, iteraciones*30)
     ax2.stem(arrayIteraciones, result)
-
+    flujo_caja_finales.append(flujoCaja[-1])
 ax.axhline(y=0, color='b', linestyle='dotted')
 
 ax.set_xlabel('N° (Número de tiradas)')
@@ -50,5 +53,36 @@ ax2.set_ylabel('fr de Ganancia')
 ax2.set_title('fr de ganar con dinero infinito según n - Sistema 3/2')
 ax2.set_xlim(left=0, right=10)
 fig2.set_facecolor('white')
+
+
+# distribucion de frecuencias absolutas del flujo de capital
+fig_dist, ax_dist = plt.subplots(figsize=(7, 4), constrained_layout=True)
+fl_no_repetidos = sorted(list(set(flujo_caja_finales)))
+fa = {}
+for value in fl_no_repetidos:
+    fa[value] = 0
+for value in flujo_caja_finales:
+    fa[value] += 1
+ax_dist.stem(list(fa.keys()), list(fa.values()))
+
+mu = np.average(flujo_caja_finales)
+sigma = np.std(flujo_caja_finales)
+print('media: ', mu)
+print('desvio: ', sigma)
+plt.axvline(x=mu, color='r', label='promedio')
+plt.axvline(x=mu+sigma, color='y', label='un desvio estandar')
+plt.axvline(x=mu-sigma, color='y')
+plt.axvline(x=mu+2*sigma, color='g', label='dos desvios estandar')
+plt.axvline(x=mu-2*sigma, color='g')
+
+ax_dist.set_ylabel('frecuencia absoluta (corridas)')
+ax_dist.set_xlabel('ganancia final')
+ax_dist.set_xlim(mu-3*sigma, mu+3*sigma)
+plt.title(
+    "Frecuencias absolutas la ganacia final luego de %d tiradas - Estrategia 3-2" % (iteraciones))
+
+plt.legend()
+plt.show()
+
 
 plt.show()
