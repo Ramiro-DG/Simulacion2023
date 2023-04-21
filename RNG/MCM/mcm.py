67403d73419d8795ea996f7e1e6ebf4a58382c65
FacundoBraida98 <facundobraida98@gmail.com>
2023-04-20 21:54:11 -0300

    Revert "fix mid_square"
    This reverts commit 5895c3a2a113fee774970b8b64f289eaac11687d.

--- RNG/MCM/mcm.py
+++ RNG/MCM/mcm.py
@@ -1,4 +1,4 @@
-def initial_seed_generator(number):
+def seed_generator(number):
     if not isinstance(number, int):
         raise ValueError("Input must be an integer.")
     number_str=str(number);
@@ -8,19 +8,19 @@ def initial_seed_generator(number):
         **dict.fromkeys(range(3, 7), int(number_str[0:-2])),
     }.get(len(number_str), 0)
     
-def mid_square(initial_seed:int ,num_iterations:int):
-  if(len(str(initial_seed)) !=4 ):
-    raise ValueError("initial_seed value must have at least 4 digits.");
+def mid_square(seed:int ,num_iterations:int):
+  if(len(str(seed)) <4 ):
+    raise ValueError("Seed value must have at least 4 digits.");
   values = [0 for _ in range(num_iterations)]
-  initial_seeds = [0 for _ in range(num_iterations)]
-  values[0] = pow(initial_seed,2)
-  initial_seeds[0]= initial_seed_generator(values[0]);
+  seeds = [0 for _ in range(num_iterations)]
+  values[0] = pow(seed,2)
+  seeds[0]= seed_generator(values[0]);
   for i in range(num_iterations-1):
     if(values[i]==0):break;
-    values[i+1]= pow(initial_seeds[i],2);
-    initial_seeds[i+1]=initial_seed_generator(values[i+1]);  
-  return initial_seed,values,initial_seeds
+    values[i+1]= pow(seeds[i],2);
+    seeds[i+1]=seed_generator(values[i+1]);  
+  return seed,values,seeds
 
-print(mid_square(1232,100))    
+print(mid_square(1221,100))    
 
   
