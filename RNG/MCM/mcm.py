45c5eba45507422728fb741376795e0f08d31c5a
FacundoBraida98 <facundobraida98@gmail.com>
2023-04-20 23:25:17 -0300

    change dictionary to else if  in function seed_generator

--- RNG/MCM/mcm.py
+++ RNG/MCM/mcm.py
@@ -1,26 +1,31 @@
-def seed_generator(number):
+def seed_generator(number:int)->int:
     if not isinstance(number, int):
         raise ValueError("Input must be an integer.")
     number_str=str(number);
-    return {
-        8: int(number_str[2:-2]),
-        7: int(number_str[1:-2]),
-        **dict.fromkeys(range(3, 7), int(number_str[0:-2])),
-    }.get(len(number_str), 0)
-    
-def mid_square(seed:int ,num_iterations:int):
-  if(len(str(seed)) <4 ):
+    if(len(number_str)==8):
+        return int(number_str[2:-2])
+    elif(len(number_str)==7):
+        return int(number_str[1:-2])
+    elif(len(number_str)<=6 and len(number_str)>=3):
+      return int(number_str[0:-2])
+    else:
+      return 0
+
+def mid_square(initial_seed:int ,num_iterations:int):
+  if(len(str(initial_seed)) <4 ):
     raise ValueError("Seed value must have at least 4 digits.");
   values = [0 for _ in range(num_iterations)]
   seeds = [0 for _ in range(num_iterations)]
-  values[0] = pow(seed,2)
+  values[0] = pow(initial_seed,2)
   seeds[0]= seed_generator(values[0]);
+  print(seeds[0]);
   for i in range(num_iterations-1):
     if(values[i]==0):break;
-    values[i+1]= pow(seeds[i],2);
+    values[i+1]=pow(seeds[i],2);
     seeds[i+1]=seed_generator(values[i+1]);  
-  return seed,values,seeds
+  return values
 
-print(mid_square(1221,100))    
+print(mid_square(1232,1000000))
+
 
   
