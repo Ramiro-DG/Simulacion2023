4e74dde3e6c81a41d53f82ef7a5bfbc5d7b8e67b
lucia cappll <luciacappll@gmail.com>
2023-04-23 20:34:33 -0300

    randu sumOverlaping test

--- RNG/test-sumOverlapping.py
+++ RNG/test-sumOverlapping.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 import GCL
 import mcm
+import randu
 
 n=1000
 
@@ -31,7 +32,6 @@ z= (max(sumas)-media)/std
 plt.plot(sumas, scipy.stats.norm.pdf(sumas, media, std), color='red', linewidth=3 )
 plt.title('Test superposicion de la suma - Generador GCL')
 plt.show()
-print('z', z)
 
 
 # Test MCM
@@ -55,6 +55,29 @@ std=np.std(sumas)
 z= (max(sumas)-media)/std
 plt.plot(sumas, scipy.stats.norm.pdf(sumas, media, std), color='red', linewidth=3 )
 plt.title('Test superposicion de la suma - Generador MCM')
+plt.show()
+
+# Test Randu
+sumas = []
+window= 50
+step= 20
+inf=0
+sup=50
+seed=1232
+seq = randu.randu(1253,1000);
+while(sup<=1000):
+    acumulador = 0
+    for n in range(inf, sup+1):
+        acumulador+= seq[n]
+    sumas.append(acumulador)
+    inf+=step
+    sup+=step
+sumas.sort()
+media = np.average(sumas)
+std=np.std(sumas)
+z= (max(sumas)-media)/std
+plt.plot(sumas, scipy.stats.norm.pdf(sumas, media, std), color='red', linewidth=3 )
+plt.title('Test superposicion de la suma - Generador Randu')
 plt.show()
 
 
