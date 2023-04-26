3914ce32d7d1c49bdabaa656f78daba89770a44a
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-04-26 15:06:19 -0300

    Implementación del test de chi cuadrado para el generador de números pseudoaleatorios de Python

--- RNG/test-chiSqr.py
+++ RNG/test-chiSqr.py
@@ -3,19 +3,13 @@ from scipy.stats import chisquare
 import GCL
 import mcm
 import randu
+import random
 
 def chi_squared_test(data, num_bins):
     observed, bins = np.histogram(data, bins=num_bins)
     expected = np.ones(num_bins) * len(data) / num_bins
     _, p_value = chisquare(observed, expected)
     return p_value
-
-
-
-
-# numeros_aleatorios = mcm.mid_square2(9731, 100)
-
-
 
 # Test GCL
 seq_gcl = GCL.generate_sequence_numbers(1000, 742895)
@@ -31,3 +25,10 @@ print('Valor de p para secuencia generada con MCM: ', p_mcm)
 seq_randu = randu.randu(1253,1000);
 p_randu = chi_squared_test(seq_randu, 10)
 print('Valor de p para secuencia generada con Randu: ', p_randu)
+
+# Test Randu
+seq_python = [];
+for _ in range(1000):
+    seq_python.append(random.random())
+p_python = chi_squared_test(seq_python, 10)
+print('Valor de p para secuencia generada con Python3: ', p_python)
